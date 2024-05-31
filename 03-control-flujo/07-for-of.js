71f294ab18c8ba8494ac94b2bce68099f6908596
ThemauJR <themau278estudio@gmail.com>
2024-05-31 06:54:47 -0700

    Add files via upload

---
+++ 03-control-flujo/07-for-of.js
@@ -0,0 +1,15 @@
+// while
+// do while
+// for
+
+let animales = ["chanchito feliz", "dragón", "perrito"];
+
+for (let animal of animales) {
+    console.log(animal);
+}
+
+let i = 0;
+while (i < animales.lenght) {
+    console.log(animales [i] );
+    i++;
+}
---
+++ 03-control-flujo/10-switch.js
@@ -0,0 +1,19 @@
+let accion = "miau";
+
+// switch ( accion) {
+//     case "listar":
+//         console.log("acción de listar");
+//         break;
+//     case "guardar":
+//         console.log("acción de guardar");
+//         break;
+//     default: 
+//         console.log("Acción no reconocida");
+// }
+
+// if y else if
+if (accion == "listar")
+    console.log("acción de listar");
+else if (accion == "guardar")
+    console.log("acción de guardar");
+    console.log("no es ninguno de los 2");
